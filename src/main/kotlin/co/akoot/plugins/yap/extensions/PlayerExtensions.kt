2058b8d7dfcbec9a59e8b64ae563756627f632e0
akoot <akoot@akoot.me>
2026-01-02 22:33:22 -0500

    update config to legacyConfig

--- src/main/kotlin/co/akoot/plugins/yap/extensions/PlayerExtensions.kt
+++ src/main/kotlin/co/akoot/plugins/yap/extensions/PlayerExtensions.kt
@@ -1,16 +1,15 @@
 package co.akoot.plugins.yap.extensions
 
-import co.akoot.plugins.bluefox.extensions.config
+import co.akoot.plugins.bluefox.extensions.legacyConfig
 import net.dv8tion.jda.api.entities.Member
-import net.kyori.adventure.audience.Audience
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 
-fun Player.isMuting(offlinePlayer: OfflinePlayer): Boolean = this.config?.getUUIDList("chat.mutedPlayers")?.contains(offlinePlayer.uniqueId) == true
-fun Player.isBlocking(member: Member): Boolean = this.config?.getLongList("chat.mutedGuildMembers")?.contains(member.idLong) == true
+fun Player.isMuting(offlinePlayer: OfflinePlayer): Boolean = this.legacyConfig.getUUIDList("chat.mutedPlayers").contains(offlinePlayer.uniqueId)
+fun Player.isBlocking(member: Member): Boolean = this.legacyConfig.getLongList("chat.mutedGuildMembers").contains(member.idLong)
 
-fun Player.mute(offlinePlayer: OfflinePlayer) = this.config?.append("chat.mutedPlayers", offlinePlayer.uniqueId)
-fun Player.block(member: Member) = this.config?.append("chat.mutedGuildMembers", member.idLong)
+fun Player.mute(offlinePlayer: OfflinePlayer) = this.legacyConfig.append("chat.mutedPlayers", offlinePlayer.uniqueId)
+fun Player.block(member: Member) = this.legacyConfig.append("chat.mutedGuildMembers", member.idLong)
 
-fun Player.unmute(offlinePlayer: OfflinePlayer) = this.config?.remove("chat.mutedPlayers", offlinePlayer.uniqueId)
-fun Player.unblock(member: Member) = this.config?.remove("chat.mutedGuildMembers", member.idLong)
+fun Player.unmute(offlinePlayer: OfflinePlayer) = this.legacyConfig.remove("chat.mutedPlayers", offlinePlayer.uniqueId)
+fun Player.unblock(member: Member) = this.legacyConfig.remove("chat.mutedGuildMembers", member.idLong)
